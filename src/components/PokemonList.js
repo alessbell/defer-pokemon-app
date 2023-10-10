0c1c210e3ffdea6a36441fcb8afa5c7cec2bf66e
Alessia Bellisario <github@bellisar.io>
2023-10-10 10:32:01 -0700

    Update header

--- src/components/PokemonList.js
+++ src/components/PokemonList.js
@@ -9,7 +9,7 @@ export default function PokemonDetails({
       <div className="mx-auto max-w-7xl px-6 lg:px-8">
         <div className="mx-auto max-w-2xl lg:mx-0">
           <h2 className="text-3xl font-bold tracking-tight text-gray-900 sm:text-4xl">
-            Our team{deferred ? " ⚡️" : " 🐌"}
+            Pokémon{deferred ? " ⚡️" : " 🐌"}
           </h2>
           <p className="mt-6 text-lg leading-8 text-gray-600">
             We’re a dynamic group of individuals who are passionate about what
