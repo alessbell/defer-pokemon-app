6e89e019d05a6a1e80f074f5bc2f259ec358c63e
Alessia Bellisario <github@bellisar.io>
2023-10-10 10:38:25 -0700

    Remove paragraph

--- src/components/PokemonList.js
+++ src/components/PokemonList.js
@@ -11,10 +11,6 @@ export default function PokemonDetails({
           <h2 className="text-3xl font-bold tracking-tight text-gray-900 sm:text-4xl">
             Pokémon{deferred ? " ⚡️" : " 🐌"}
           </h2>
-          <p className="mt-6 text-lg leading-8 text-gray-600">
-            We’re a dynamic group of individuals who are passionate about what
-            we do and dedicated to delivering the best results for our clients.
-          </p>
         </div>
         <ul className="mx-auto mt-20 grid max-w-2xl grid-cols-2 gap-y-16 gap-x-8 text-center sm:grid-cols-3 md:grid-cols-4 lg:mx-0 lg:max-w-none">
           {pokemon?.map((pokemon) => (
